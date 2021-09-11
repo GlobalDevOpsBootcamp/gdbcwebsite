89188853ea0898db200b0461173d776ff8f1a80e
Michiel van Oudheusden <mvanoudheusden@xpirit.com>
2021-09-11 23:42:02 +0200

    cleanup

--- Gemfile
+++ Gemfile
@@ -1,4 +1,3 @@
 source 'https://rubygems.org'
 gem 'jekyll'
 gem 'jekyll-seo-tag'
-gem "jekyll-github-metadata"
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,52 +6,55 @@
  * Run '$ npm run css:dev' to compile changes in this file.
  */
 const colors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
     darkMode: 'class',
     purge: [
         '{,!(node_modules|_site)/**/}*.{html,md}',
     ],
-    extend: {
-        lineClamp: {
-            10: '10'
-        },
-        spacing: {
-            128: '32rem',
-            160: '40rem'
-        },
-        container: {
-            center: true,
-            padding: {
-                DEFAULT: '1rem',
-                lg: '1.5rem'
+    theme: {
+        extend: {
+            lineClamp: {
+                10: '10'
             },
-            screens: {
-                lg: '1152px'
-            }
-        },
-        colors: {
-            turqoise: colors.cyan,
-            gray: colors.blueGray,
-            'dark-alternate': '#212224',
-            'dark-200': '#1F2129',
-            'dark-300': '#2B2D33',
-            'gdbc-orange': '#ef8300'
-        },
-        borderRadius: {
-            medium: '0.312rem'
-        },
-        boxShadow: {
-            drawer: '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
-            testimonial: '0 63px 50px 0 rgba(0,0,0,0.07)',
-            blue: '0 0px 60px 15px rgba(55, 125, 255, 0.1)'
-        },
-        gradientColorStops: ['group-hover']
+            spacing: {
+                128: '32rem',
+                160: '40rem'
+            },
+            container: {
+                center: true,
+                padding: {
+                    DEFAULT: '1rem',
+                    lg: '1.5rem'
+                },
+                screens: {
+                    lg: '1152px'
+                }
+            },
+            colors: {
+                turqoise: colors.cyan,
+                gray: colors.blueGray,
+                'dark-alternate': '#212224',
+                'dark-200': '#1F2129',
+                'dark-300': '#2B2D33',
+                'gdbc-orange': '#ef8300'
+            },
+            borderRadius: {
+                medium: '0.312rem'
+            },
+            boxShadow: {
+                drawer: '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
+                testimonial: '0 63px 50px 0 rgba(0,0,0,0.07)',
+                blue: '0 0px 60px 15px rgba(55, 125, 255, 0.1)'
+            },
+            gradientColorStops: ['group-hover'],
+            fontFamily: {
+                sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+              },
+        }
     },
-    fontFamily: {
-        sans: 'Raleway',
-        inter: 'Inter, sans-serif'
-    },
+   
     plugins: [
         require('@tailwindcss/typography'),
     ],
